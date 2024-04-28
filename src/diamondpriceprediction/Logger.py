3ebb55b70fc8bfd5a0f247436eead3814caa1985
ankitguar0 <ankitparashar000@gmail.com>
2024-04-28 12:41:24 +0530

    commit the logger file code

---
+++ src/diamondpriceprediction/Logger.py
@@ -0,0 +1,14 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# logpath uses the path.join functions to make the folder where file will be save
+logpath=os.path.join(os.getcwd(),"logs")
+
+# now make the direcotry
+os.makedirs(logpath,exist_ok=True)
+
+# now put the file into path
+Log_filepath=os.path.join(logpath,LOG_FILE)
