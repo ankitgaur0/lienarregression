60038f4740065c293e4d0f2c3b10376e59ca1137
ankitguar0 <ankitparashar000@gmail.com>
2024-05-23 17:04:48 +0530

    make changes in requiements and setup.py file because it was showing some error related to import src

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import setup,find_packages
-import os
+from setuptools import find_packages,setup
+from typing import List
 
-setup (
-    name="diamondpriceprediction",
-    versions='0.0.01',
-    author="Ankit_gaur",
-    author_email="ankitparashar000@gmail.com",
-    install_requires=["pandas","numpy","scikit-learn","matplotlib","seaborn"],
-    #install_requires=get_requirements=("requiements.txt"),
+"""HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements"""
+
+setup(
+    name='DimondPricePrediction',
+    version='0.0.1',
+    author='sunny savita',
+    author_email='sunny.savita@ineuron.ai',
+    install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
